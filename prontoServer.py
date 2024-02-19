edc57a896430c9f1cb51ff7ab12e38eba4d8d887
Quantum-Otter <Robbiehawkins1012@gmail.com>
2024-02-18 23:29:09 -0700

    added basic post request handling

--- prontoServer.py
+++ prontoServer.py
@@ -2,13 +2,18 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods = ["POST"])
 def hello_world():
     if(request.method == "GET"):
 
         #KEEP THIS IN MIND FOR SMS VS APP TRANSACTIONS
         print(f"FUCK YEA {request.base_url}")
         return "<p>Hi Josh :)</p>"
+    elif(request.method == "POST"):
+
+        print(f"POSTIN TIME {request.base_url}")
+        print(f"request data: {request.values["test"]}")
+        return "<p>POST</p>"
     else:
         return "<p>Hello, World!</p>"
 
---
+++ requestTester.py
@@ -0,0 +1,3 @@
+import requests
+
+requests.post('http://127.0.0.1:5000', data = {"test": "TEST_DATA_AAAA"})
