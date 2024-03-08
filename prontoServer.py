68e1e8d2bfddccc1acba7f02871b4b8cd8d38076
Quantum-Otter <Robbiehawkins1012@gmail.com>
2024-03-08 16:49:59 -0700

    feat: add database support

--- prontoServer.py
+++ prontoServer.py
@@ -1,23 +1,43 @@
 from flask import Flask, request
+import mysql.connector;
+import os;
+
 
 app = Flask(__name__)
 
-total = 0
+conn = mysql.connector.MySQLConnection(
+    user = "root",
+    password = os.getenv('MySqlPassword'),
+    database="uccs_pronto"
+    )
+conn.autocommit = False;
 
 @app.route("/", methods = ["GET","POST"])
 def donation_occured():
 
-    global total
-
     if(request.method == "GET"):
-        return f"{total}"
+        display_string = ""
+        value = submit_query("SELECT user_id, SUM(donation_amount) from donators GROUP BY user_id")
+
+        for (user_id, donation_amount) in value:
+            display_string += f"<div>Sum of user {user_id}: {donation_amount}<div>\n"
+        
+        value = submit_query("SELECT SUM(donation_amount) from donators")
+
+        display_string += f"<br><div>Total Sum: {value[0][0]}<div>"
+
+        return f"{display_string}"
     
 
     elif(request.method == "POST"):
         
-        if(type(request.json["donationAmount"]) is int ):
+        if(type(request.json["user_id"]) is int 
+           and type(request.json["amount"]) is float):
+            
             print(f"request data: {request.json}")
-            total += request.json["donationAmount"]
+            
+            submit_query(f"INSERT INTO donators (user_id, donation_amount) values ({request.json["user_id"]},{request.json["amount"]})")
+
         else:
             print("SHHSH")
 
@@ -29,3 +49,16 @@ def donation_occured():
 @app.route("/DEV_ONLY")
 def display_my_secret():
     return "<p>My secret place >:)</p>"
+
+def submit_query(query):
+    global conn
+
+    cur = conn.cursor()
+    cur.execute(query)
+
+    result = cur.fetchall()
+
+    conn.commit()
+    cur.close()
+
+    return result
